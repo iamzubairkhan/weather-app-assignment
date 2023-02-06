fbbddb799b0d5771505132ee35197bb539da160b
Zubair khan <mzubair.ciit@gmail.com>
2023-02-06 11:18:43 +0100

    Offload network operations to background thread

--- app/src/main/java/com/example/weatherappassignment/data/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherappassignment/data/WeatherRepository.kt
@@ -3,6 +3,8 @@ package com.example.weatherappassignment.data
 import com.example.weatherappassignment.data.Result.Error
 import com.example.weatherappassignment.data.Result.Success
 import com.example.weatherappassignment.data.model.Weather
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 interface WeatherRepository {
@@ -10,11 +12,12 @@ interface WeatherRepository {
 }
 
 class WeatherRepositoryImpl @Inject constructor(private val weatherDataSource: WeatherDataSource) : WeatherRepository {
-    override suspend fun getCurrentWeather(location: String): Result<Weather> =
+    override suspend fun getCurrentWeather(location: String): Result<Weather> = withContext(Dispatchers.IO) {
         try {
             val weather = weatherDataSource.getCurrentWeather(location)
             Success(data = weather)
         } catch (exception: Exception) {
             Error(exception)
         }
+    }
 }
